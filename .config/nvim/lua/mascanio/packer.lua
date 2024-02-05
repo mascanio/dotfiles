b00285a260b8568eb2953c64120e33343aa3cacf
Miguel Ascanio Gómez <mascanio13@gmail.com>
2024-02-05 17:27:22 +0100

    [nvim] add fugitive

--- .config/nvim/lua/mascanio/packer.lua
+++ .config/nvim/lua/mascanio/packer.lua
@@ -18,4 +18,6 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+
 end)
