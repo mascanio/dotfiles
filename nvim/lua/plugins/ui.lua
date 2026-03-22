277ce65ca9f5e8edbd831393996acc1610552599
Miguel Ascanio Gómez <mascanio13@gmail.com>
2026-03-22 18:25:10 +0100

    lualine

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -31,8 +31,103 @@ return {
   },
   {
     "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {},
+    init = function()
+      vim.g.lualine_laststatus = vim.o.laststatus
+      if vim.fn.argc(-1) > 0 then
+        vim.o.statusline = " "
+      else
+        vim.o.laststatus = 0
+      end
+    end,
+    opts = function()
+      local lualine_require = require("lualine_require")
+      lualine_require.require = require
+
+      vim.o.laststatus = vim.g.lualine_laststatus
+
+      ---@param name string
+      ---@return string?
+      local function fg(name)
+        local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
+        return hl and hl.fg and string.format("#%06x", hl.fg)
+      end
+
+      return {
+        options = {
+          theme = "auto",
+          globalstatus = vim.o.laststatus == 3,
+          disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch" },
+          lualine_c = {
+            {
+              function() return "󱉭 " .. vim.fn.fnamemodify(vim.uv.cwd() or "", ":t") end,
+              color = function() return { fg = fg("Special") } end,
+            },
+            {
+              "diagnostics",
+              symbols = {
+                error = " ",
+                warn  = " ",
+                info  = " ",
+                hint  = " ",
+              },
+            },
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            { "filename", path = 1 },
+          },
+          lualine_x = {
+            -- stylua: ignore start
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = function() return { fg = fg("Statement") } end,
+            },
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = function() return { fg = fg("Constant") } end,
+            },
+            {
+              require("lazy.status").updates,
+              cond = require("lazy.status").has_updates,
+              color = function() return { fg = fg("Special") } end,
+            },
+            -- stylua: ignore end
+            {
+              "diff",
+              symbols = {
+                added    = " ",
+                modified = " ",
+                removed  = " ",
+              },
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
+          },
+          lualine_y = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+          },
+          lualine_z = {
+            function() return " " .. os.date("%R") end,
+          },
+        },
+        extensions = { "neo-tree", "lazy" },
+      }
+    end,
   },
   {
     "folke/snacks.nvim",
