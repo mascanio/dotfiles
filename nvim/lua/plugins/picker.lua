24981f49846310bf4c982c3ca379f2104510f11b
Miguel Ascanio Gómez <mascanio13@gmail.com>
2026-04-18 18:20:21 +0200

    picker

--- nvim/lua/plugins/picker.lua
+++ nvim/lua/plugins/picker.lua
@@ -1,4 +1,7 @@
-local cycle_layouts = { "telescope", "ivy", "vscode" }
+local file_layouts = { "vscode", "telescope", "ivy" }
+local default_layouts = { "telescope", "ivy" }
+
+local vscode_sources = { files = true, git_files = true, recent = true, smart = true }
 
 return {
   {
@@ -16,16 +19,33 @@ return {
         },
         actions = {
           cycle_layout = function(picker)
+            local layouts = vscode_sources[picker.opts.source] and file_layouts or default_layouts
             local idx = picker._layout_idx or 1
-            idx = idx % #cycle_layouts + 1
+            idx = idx % #layouts + 1
             picker._layout_idx = idx
-            picker:set_layout(cycle_layouts[idx])
+            picker._layout_preset = layouts[idx]
+            picker:set_layout(layouts[idx])
+          end,
+          smart_esc = function(picker)
+            local mode = vim.api.nvim_get_mode().mode
+            if picker._layout_preset == "vscode" or mode == "n" then
+              picker:close()
+            else
+              vim.cmd("stopinsert")
+            end
           end,
         },
+        on_show = function(picker)
+          if not picker._layout_preset then
+            picker._layout_preset = vscode_sources[picker.opts.source] and "vscode" or "telescope"
+            picker._layout_idx = vscode_sources[picker.opts.source] and 3 or 1
+          end
+        end,
         win = {
           input = {
             keys = {
               ["<a-l>"] = { "cycle_layout", mode = { "n", "i" } },
+              ["<Esc>"] = { "smart_esc", mode = { "n", "i" } },
             },
           },
         },
